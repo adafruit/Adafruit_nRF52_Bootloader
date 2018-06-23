2872eb899392d1f8c496a47d663c76a8497f39b8
hathach <thach@tinyusb.org>
2018-06-23 14:15:39 +0700

    clean up

--- src/usb/uf2/uf2cfg.h
+++ src/usb/uf2/uf2cfg.h
@@ -1,15 +1,15 @@
-#define UF2_VERSION "1.00"
-#define PRODUCT_NAME "Adafruit Bluefruit nRF52"
-#define BOARD_ID "NRF52-Bluefruit-v0"
-#define INDEX_URL "https://www.adafruit.com/product/0000"
-#define BOOTLOADER_ID  MK_DIS_FIRMWARE
+#define UF2_VERSION        "1.00"
+#define PRODUCT_NAME       "Adafruit Bluefruit nRF52"
+#define BOARD_ID           "NRF52-Bluefruit-v0"
+#define INDEX_URL          "https://www.adafruit.com/product/0000"
+#define BOOTLOADER_ID      MK_DIS_FIRMWARE
 
-#define UF2_NUM_BLOCKS 8000   // at least 4,1 MB for FAT16
-#define VOLUME_LABEL "NRF52BOOT  "
-#define FLASH_SIZE (10*1024) //(USER_FLASH_END-USER_FLASH_START)
+#define UF2_NUM_BLOCKS     8000   // at least 4,1 MB for FAT16
+#define VOLUME_LABEL       "NRF52BOOT  "
+#define FLASH_SIZE         (USER_FLASH_END-USER_FLASH_START)
 
 // Only allow to write application
-#define USER_FLASH_START 0x26000
-#define USER_FLASH_END   0xAD000 // Fat Fs start here
+#define USER_FLASH_START   0x26000
+#define USER_FLASH_END     0xAD000 // Fat Fs start here
 
-#define FLASH_PAGE_SIZE 4096
+#define FLASH_PAGE_SIZE    4096
