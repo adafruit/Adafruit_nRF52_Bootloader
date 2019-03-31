79fe6cca38a003d442e78ce93e5a9a86deb38c45
Charles R. Portwood II <charlesportwoodii@erianna.com>
2019-03-31 09:19:53 -0500

    Adding board definition for SparkFun Pro nRF52840 Mini

---
+++ src/boards/sparkfun_pro_nrf52840_mini.h
@@ -0,0 +1,76 @@
+/**************************************************************************/
+/*!
+    @file     sparkfun_pro_nrf52840_mini.h
+    @author   charlesportwoodii (https://www.erianna.com)
+
+    @section LICENSE
+
+    Software License Agreement (BSD License)
+
+    Copyright (c) 2019, Adafruit Industries (adafruit.com)
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+    1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holders nor the
+    names of its contributors may be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
+    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+/**************************************************************************/
+
+#ifndef SPARKFUN_PRO_NRF52840_MINI_H
+#define SPARKFUN_PRO_NRF52840_MINI_H
+
+/*------------------------------------------------------------------*/
+/* LED
+ *------------------------------------------------------------------*/
+#define LEDS_NUMBER       1
+#define LED_PRIMARY_PIN   7  // Blue
+#define LED_STATE_ON      0
+
+/*------------------------------------------------------------------*/
+/* BUTTON
+ *------------------------------------------------------------------*/
+#define BUTTONS_NUMBER 2
+
+#define BUTTON_1       13
+#define BUTTON_2       2
+#define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
+
+/*------------------------------------------------------------------*/
+/* UART
+ *------------------------------------------------------------------*/
+#define RX_PIN_NUMBER  15
+#define TX_PIN_NUMBER  17
+#define CTS_PIN_NUMBER 19
+#define RTS_PIN_NUMBER 20
+#define HWFC           false
+
+// Used as model string in OTA mode
+#define DIS_MANUFACTURER  "SparkFun"
+#define DIS_MODEL         "DEV-15025"
+
+#define PRODUCT_NAME      "SparkFun Pro nRF52840 Mini"
+#define VOLUME_LABEL      "SRPKFNP840M"
+
+#define BOARD_ID          "SparkFun-Pro-nRF52840-Mini"
+
+#define INDEX_URL         "https://www.sparkfun.com/products/15025"
+
+#endif // SPARKFUN_PRO_NRF52840_MINI_H
