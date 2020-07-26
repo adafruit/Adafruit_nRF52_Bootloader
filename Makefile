adafruit/Adafruit_nRF52_Bootloader

commit cf4efa2c94098908f127699746b5cb9d1538df1d

move boards.c/h into boards folder

Makefile
@@ -95,6 +95,9 @@ endif
 # all files in src
 C_SRC += $(wildcard src/*.c)
 
+# all files in boards
+C_SRC += $(wildcard src/boards/*.c)
+
 # all sources files in specific board
 C_SRC += $(wildcard src/boards/$(BOARD)/*.c)
 
@@ -164,11 +167,10 @@ ASM_SRC = $(NRFX_PATH)/mdk/gcc_startup_$(MCU_SUB_VARIANT).S
 
 # src
 IPATH += src
+IPATH += src/boards
 IPATH += src/boards/$(BOARD)
-
 IPATH += src/cmsis/include
 IPATH += src/usb
-IPATH += src/boards
 IPATH += $(TUSB_PATH)
 
 # nrfx

commit 1a138da50d34f2b0e44fc092525dce6ee3239bd3

Exclude non-board files from list of valid boards.
Fixes #153.
Regression caused by commit cf4efa2c94098908f127699746b5cb9d1538df1d.

Makefile
@@ -63,7 +63,8 @@ GDB_BMP = $(GDB) -ex 'target extended-remote $(BMP_PORT)' -ex 'monitor swdp_scan
 #---------------------------------
 # Select the board to build
 #---------------------------------
-BOARD_LIST = $(sort $(subst src/boards/,,$(wildcard src/boards/*)))
+# Note: whitespace is not allowed in the filenames... it WILL break this part of the script
+BOARD_LIST = $(sort $(filter-out boards.h boards.c,$(notdir $(wildcard src/boards/*))))
 
 ifeq ($(filter $(BOARD),$(BOARD_LIST)),)
   $(info You must provide a BOARD parameter with 'BOARD='. Supported boards are:)