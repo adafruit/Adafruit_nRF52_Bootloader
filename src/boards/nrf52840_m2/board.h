a5b78ff4103b16f71ffdb3a0981292be4786b332
nitz <nitz@users.noreply.github.com>
2020-08-06 11:11:31 -0400

    Updated with @xiongyhui's VID/PID from Seeed

--- src/boards/nrf52840_m2/board.h
+++ src/boards/nrf52840_m2/board.h
@@ -56,9 +56,9 @@
 //--------------------------------------------------------------------+
 // USB
 //--------------------------------------------------------------------+
-#define USB_DESC_VID            0x1915
-#define USB_DESC_UF2_PID        0x0029
-#define USB_DESC_CDC_ONLY_PID   0x002A
+#define USB_DESC_VID            0x2886
+#define USB_DESC_UF2_PID        0xF00F
+#define USB_DESC_CDC_ONLY_PID   0xF00F
 
 //--------------------------------------------------------------------+
 // UF2
