498ab10ef77e1edaae91bab91050e252551dcb23
zhengchangzhou <zhengchang.zzc@alibaba-inc.com>
2021-10-07 23:04:43 +0800

    优化bootloader

--- src/boards/keystar/board.h
+++ src/boards/keystar/board.h
@@ -29,23 +29,23 @@
 /* LED
  *------------------------------------------------------------------*/
 #define LEDS_NUMBER         2
-#define LED_PRIMARY_PIN     13
-#define LED_SECONDARY_PIN   14
+#define LED_PRIMARY_PIN     _PINNUM(0, 09)
+#define LED_SECONDARY_PIN   _PINNUM(0, 10)
 #define LED_STATE_ON        0
 
 /*------------------------------------------------------------------*/
 /* BUTTON
  *------------------------------------------------------------------*/
 #define BUTTONS_NUMBER      2
-#define BUTTON_1            11
-#define BUTTON_2            12
+#define BUTTON_1              _PINNUM(0, 1)
+#define BUTTON_2              _PINNUM(0, 18)
 #define BUTTON_PULL         NRF_GPIO_PIN_PULLUP
 
 //--------------------------------------------------------------------+
 // BLE OTA
 //--------------------------------------------------------------------+
-#define BLEDIS_MANUFACTURER    "Nordic"
-#define BLEDIS_MODEL           "PCA10056"
+#define BLEDIS_MANUFACTURER    "KEYSTAR"
+#define BLEDIS_MODEL           "Orion-OTA"
 
 //--------------------------------------------------------------------+
 // USB
@@ -56,8 +56,8 @@
 #define USB_DESC_UF2_PID       0x0029
 #define USB_DESC_CDC_ONLY_PID  0x0029
 
-#define UF2_PRODUCT_NAME    "Nordic nRF52840 DK"
-#define UF2_BOARD_ID        "nRF52840-pca10056-v1"
+#define UF2_PRODUCT_NAME    "KEYSTAR"
+#define UF2_BOARD_ID        "Orion"
 #define UF2_INDEX_URL       "https://www.nordicsemi.com/Software-and-Tools/Development-Kits/nRF52840-DK"
 
 #endif // PCA10056_H
