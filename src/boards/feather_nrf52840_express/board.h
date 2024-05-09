0bc8a8993f6b7b9a175bc1ba9aafa26f5e534122
hathach <thach@tinyusb.org>
2024-05-09 20:15:16 +0700

    - add P1.14 as NEOPIXEL_POWER_PIN for feather nrf52840 revE - remove linkermap from ci

--- src/boards/feather_nrf52840_express/board.h
+++ src/boards/feather_nrf52840_express/board.h
@@ -36,6 +36,7 @@
 #define LED_STATE_ON          1
 
 #define LED_NEOPIXEL          _PINNUM(0, 16)
+#define NEOPIXEL_POWER_PIN    _PINNUM(1, 14)
 #define NEOPIXELS_NUMBER      1
 #define BOARD_RGB_BRIGHTNESS  0x040404
 
@@ -63,7 +64,7 @@
 //------------- UF2 -------------//
 #define UF2_PRODUCT_NAME      "Adafruit Feather nRF52840 Express"
 #define UF2_VOLUME_LABEL      "FTHR840BOOT"
-#define UF2_BOARD_ID          "nRF52840-Feather-revD"
+#define UF2_BOARD_ID          "nRF52840-Feather-revE"
 #define UF2_INDEX_URL         "https://www.adafruit.com/product/4062"
 
 #endif // _FEATHER_NRF52840_H
