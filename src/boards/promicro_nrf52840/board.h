9027359cec40e8d32e42153b86554b0e26ffedb9
Patrick Cook <qphysicscook@gmail.com>
2024-09-08 17:11:05 -0400

    add support for promicro/supermini nrf52 boards

---
+++ src/boards/promicro_nrf52840/board.h
@@ -0,0 +1,66 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Ha Thach for Adafruit Industries
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef _PROMICRO_NRF52840
+#define _PROMICRO_NRF52840
+
+#define _PINNUM(port, pin)    ((port)*32 + (pin))
+
+/*------------------------------------------------------------------*/
+/* LED
+ *------------------------------------------------------------------*/
+#define LEDS_NUMBER           1
+#define LED_PRIMARY_PIN       _PINNUM(0, 15)
+#define LED_STATE_ON          1 // State when LED is lit (active HIGH)
+
+#define NEOPIXELS_NUMBER      0
+
+/*------------------------------------------------------------------*/
+/* BUTTON
+ *------------------------------------------------------------------*/
+#define BUTTONS_NUMBER        2
+#define BUTTON_1              _PINNUM(0, 14) // RESET
+#define BUTTON_2              _PINNUM(0, 3) // NC
+#define BUTTON_PULL           NRF_GPIO_PIN_PULLUP
+
+//--------------------------------------------------------------------+
+// BLE OTA
+//--------------------------------------------------------------------+
+#define BLEDIS_MANUFACTURER   "ProMicro"
+#define BLEDIS_MODEL          "ProMicro nRF52840"
+
+//--------------------------------------------------------------------+
+// USB
+//--------------------------------------------------------------------+
+#define USB_DESC_VID           0x1209
+#define USB_DESC_UF2_PID       0x0064
+#define USB_DESC_CDC_ONLY_PID  0x0064
+
+//------------- UF2 -------------//
+#define UF2_PRODUCT_NAME   "ProMicro nRF52840"
+#define UF2_VOLUME_LABEL   "PROMICRO"
+#define UF2_BOARD_ID       "ProMicro-nRF52840"
+#define UF2_INDEX_URL      "https://github.com/joric/nrfmicro/wiki"
+
+#endif // _PROMICRO_NRF52840
