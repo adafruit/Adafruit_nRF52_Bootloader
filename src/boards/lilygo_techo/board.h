f3f9205b7febe08ce2a6dbb1201528b5d1e44286
Florian <florian@voit.li>
2026-04-04 16:03:11 +0200

    Add files via upload

---
+++ src/boards/lilygo_techo/board.h
@@ -0,0 +1,65 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Yihui Xiong for Makerdiary
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef _TECHO
+#define _TECHO
+
+/*------------------------------------------------------------------*/
+/* LED
+ *------------------------------------------------------------------*/
+#define LEDS_NUMBER     2
+#define LED_PRIMARY_PIN PINNUM(0, 13) // Red
+#define LED_SECONDARY_PIN PINNUM(0, 15) // Blue
+#define LED_STATE_ON    0
+
+/*------------------------------------------------------------------*/
+/* BUTTON
+ *------------------------------------------------------------------*/
+#define BUTTON_DFU     PINNUM(1, 9)  // Primary Button
+#define BUTTON_DFU_OTA PINNUM(0, 18) // unusable: RESET
+#define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
+
+//--------------------------------------------------------------------+
+// BLE OTA
+//--------------------------------------------------------------------+
+#define BLEDIS_MANUFACTURER "Lilygo"
+#define BLEDIS_MODEL        "T-Echo"
+
+//--------------------------------------------------------------------+
+// USB
+//--------------------------------------------------------------------+
+#define USB_DESC_VID          0x239A
+#define USB_DESC_UF2_PID      0x0029
+#define USB_DESC_CDC_ONLY_PID 0x0029
+
+//--------------------------------------------------------------------+
+// UF2
+//--------------------------------------------------------------------+
+#define UF2_PRODUCT_NAME "Lilygo T-Echo"
+#define UF2_VOLUME_LABEL "T-Echo"
+#define UF2_BOARD_ID     "nRF52840-T-Echo"
+#define UF2_INDEX_URL    "https://lilygo.cc/"
+
+
+#endif /* _TECHO */
