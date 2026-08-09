eddc9d2a76d07b5882cf290ca2e125a4753ff8c5
Justin Gerace <justin@gerace.io>
2026-08-08 20:35:45 -0700

    Add Sparkfun Pro nRF52840 Mini board

---
+++ src/boards/sparkfun_nrf52840_mini/board.h
@@ -0,0 +1,62 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Ha Thach for Adafruit Industries
+ * Copyright (c) 2026 Justin Gerace
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef _SPARKFUN_NRF52840_MINI_H_
+#define _SPARKFUN_NRF52840_MINI_H_
+
+/*------------------------------------------------------------------*/
+/* LED
+ *------------------------------------------------------------------*/
+#define LEDS_NUMBER           1
+#define LED_PRIMARY_PIN       PINNUM(0, 7)
+#define LED_STATE_ON          1
+
+/*------------------------------------------------------------------*/
+/* BUTTON
+ *------------------------------------------------------------------*/
+#define BUTTON_DFU            PINNUM(0, 13)       // Primary Button
+#define BUTTON_DFU_OTA        PINNUM(0, 18)       // unusable: RESET
+#define BUTTON_PULL           NRF_GPIO_PIN_PULLUP
+
+//--------------------------------------------------------------------+
+// BLE OTA
+//--------------------------------------------------------------------+
+#define BLEDIS_MANUFACTURER   "SparkFun"
+#define BLEDIS_MODEL          "nRF52840 Mini"
+
+//--------------------------------------------------------------------+
+// USB
+//--------------------------------------------------------------------+
+#define USB_DESC_VID          0x1B4F
+#define USB_DESC_UF2_PID      0x0029
+#define USB_DESC_CDC_ONLY_PID 0x002A
+
+//------------- UF2 -------------//
+#define UF2_PRODUCT_NAME      "SparkFun Pro nRF52840 Mini"
+#define UF2_VOLUME_LABEL      "SFNRF52BOOT"
+#define UF2_BOARD_ID          "SparkFun-Pro-nRF52840-Mini"
+#define UF2_INDEX_URL         "https://www.sparkfun.com/products/15025"
+
+#endif /* _SPARKFUN_NRF52840_MINI_H_ */
